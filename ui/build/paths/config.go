8f9e53206365d3c0228b062ddb4658771cce1ee9
Pavel Dubrova <pashadubrova@gmail.com>
2020-05-31 12:03:14 +0300

    build: Whitelist nproc
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -95,6 +95,7 @@ var Configuration = map[string]PathConfig{
 	"lsof":     Allowed,
 	"m4":       Allowed,
 	"make":     Allowed,
+	"nproc":    Allowed,
 	"openssl":  Allowed,
 	"patch":    Allowed,
 	"pstree":   Allowed,
