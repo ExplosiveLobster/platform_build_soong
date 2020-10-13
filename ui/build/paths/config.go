7bdbcdbf85eda6a826ced08ca66ccf4ff63cdb5e
Pavel Dubrova <pashadubrova@gmail.com>
2020-10-13 07:45:11 +0300

    build: Whitelist nproc
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -96,6 +96,7 @@ var Configuration = map[string]PathConfig{
 	"unzip":   Allowed,
 	"zip":     Allowed,
 	"make":    Allowed,
+	"nproc":   Allowed,
 
 	// Host toolchain is removed. In-tree toolchain should be used instead.
 	// GCC also can't find cc1 with this implementation.
